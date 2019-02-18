61fd75725634111113fe527b9098cdea3cda9730
Paula Silva <paula.r.silva@inesctec.pt>
2019-02-18 19:28:55 +0000

    test

--- R/miceDS.R
+++ R/miceDS.R
@@ -285,8 +285,8 @@ miceDS <- function(datasource, m = 5,
   # }
   # x.data.frame <- data.frame(x.vars)
   # 
+  datatext <- paste0("data.frame(",datasource,")")
+  dataset <- eval(parse(text=datatext))
   
-  teste <- eval(parse(text=datasource))
-  
-  return(teste)
+  return(dataset)
 }
