fda0f83e7485349cc38da55047eebed9d33c79d5
Paula Silva <paula.r.silva@inesctec.pt>
2019-03-08 16:25:32 +0000

    test

--- R/miceDS.R
+++ R/miceDS.R
@@ -288,7 +288,7 @@ miceDS <- function(datasource, m = 5,
     # blocks lead
     blocks <- make.blocksDS(colnames(data))
     predictorMatrix <- make.predictorMatrixDS(data, blocks)
-    formulas <- make.formulas(data, blocks)
+    formulas <- make.formulasDS(data, blocks)
     #return(blocks)
   }
   
