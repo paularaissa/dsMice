6f80debf6e51054191db463a79fe9b38ccb832e1
Paula Silva <paula.r.silva@inesctec.pt>
2019-03-20 19:00:08 +0000

    test

--- R/getImpute.R
+++ R/getImpute.R
@@ -46,7 +46,7 @@ getImpute <- function(beta, formula) {
   xValuesMiss <- as.data.frame(bindxy[,naRows])
   
   # #Select subset of xValues
-  xValues <- unique(bindxy)
+  xValues <- unique(subset(x=bindxy, select=vars[-1]))
   
   # #xValues <- as.data.frame(xValues[!is.na(xValues)])
   # 
