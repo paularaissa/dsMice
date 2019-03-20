110c6685f85d38c9eefbb76e9a1a0cc8d3ccc6cb
Paula Silva <paula.r.silva@inesctec.pt>
2019-03-20 16:57:43 +0000

    test

--- R/getImpute.R
+++ R/getImpute.R
@@ -41,38 +41,40 @@ getImpute <- function(beta, formula) {
   #Select subset of missing data
   xValuesMiss <- subset(x=naRows, select=formatedVars[-1])
   
-  #Select subset of xValues
-  xValues <- unique(subset(x=bindxy, select=formatedVars[-1]))
-  #xValues <- as.data.frame(xValues[!is.na(xValues)])
+  return(xValuesMiss)
   
-  #Formula to compute the estimated values
-  xMiss <- as.matrix(xValuesMiss)
-  estimated <- xMiss %*% as.vector(beta.reg[-1])
-    
-  # #Difference between estimates and real values
-  cont <- 1
-  imputedValues <- c()
-  for (value in estimated) {
-     subtract <- data.frame(mapply('-', value, xValues)) #same x values rownames 
-     colnames(subtract) <- "dif"
-     rownames(subtract) <- rownames(xValues)
-     top5 <- subtract[order(subtract$dif)[1:5],]
-     randomValue <- sample(top5, 1)
-     matching <- match(randomValue, subtract$dif)
-     names <- rownames(subtract) #search the corresponding rowname
-     idValor <- names[matching]
-     valor <- xValues[idValor, ]
-     imputedValues[cont] <- valor
-     cont <- cont + 1
-  }
-  imputedValues <- as.data.frame(imputedValues)
-  rownames(imputedValues) <- rownames(naRows)
-  
-  # estimated <- data.frame(estimated, missPosition)
+  # #Select subset of xValues
+  # xValues <- unique(subset(x=bindxy, select=formatedVars[-1]))
+  # #xValues <- as.data.frame(xValues[!is.na(xValues)])
   # 
-  # vars <- all.vars(as.formula(formula))
-  # histogram <- dsMice::getHistogram(paste0("D$", vars[2]))
-  
-  return(imputedValues)
+  # #Formula to compute the estimated values
+  # xMiss <- as.matrix(xValuesMiss)
+  # estimated <- xMiss %*% as.vector(beta.reg[-1])
+  #   
+  # # #Difference between estimates and real values
+  # cont <- 1
+  # imputedValues <- c()
+  # for (value in estimated) {
+  #    subtract <- data.frame(mapply('-', value, xValues)) #same x values rownames 
+  #    colnames(subtract) <- "dif"
+  #    rownames(subtract) <- rownames(xValues)
+  #    top5 <- subtract[order(subtract$dif)[1:5],]
+  #    randomValue <- sample(top5, 1)
+  #    matching <- match(randomValue, subtract$dif)
+  #    names <- rownames(subtract) #search the corresponding rowname
+  #    idValor <- names[matching]
+  #    valor <- xValues[idValor, ]
+  #    imputedValues[cont] <- valor
+  #    cont <- cont + 1
+  # }
+  # imputedValues <- as.data.frame(imputedValues)
+  # rownames(imputedValues) <- rownames(naRows)
+  # 
+  # # estimated <- data.frame(estimated, missPosition)
+  # # 
+  # # vars <- all.vars(as.formula(formula))
+  # # histogram <- dsMice::getHistogram(paste0("D$", vars[2]))
+  # 
+  # return(imputedValues)
   
 }
