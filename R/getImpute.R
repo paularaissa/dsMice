e500ab47fef23a0a79f7f75e1d15f49fefdf1e48
Paula Silva <paula.r.silva@inesctec.pt>
2019-03-25 12:13:31 +0000

    test

--- R/getImpute.R
+++ R/getImpute.R
@@ -43,9 +43,9 @@ getImpute <- function(beta, formula) {
   xValuesMiss <- bindxy[naLines,]
   
   #Select subset of complete data
-  xValuesComplete <- bindxy[-naLines, ]
+  #xValuesComplete <- bindxy[-naLines, ]
   
-  return(list(miss=xValuesMiss, comp=xValuesComplete))
+  return(list(miss=xValuesMiss))
   
   # #Select subset of xValues
   # <- vars[-1]
