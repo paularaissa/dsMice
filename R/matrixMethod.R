87961fde73d46b428ed77925523162ea21d49a19
Paula Silva <paula.r.silva@inesctec.pt>
2019-04-02 19:58:50 +0100

    test

--- R/matrixMethod.R
+++ R/matrixMethod.R
@@ -36,16 +36,15 @@
 matrixMethod <- function(formula, subset=NULL, weight=1, family=NULL) {
   bindxy <- getVarbyFormula(formula, subset, weight, family)
   
-  # bind.x <- data.matrix(bindxy[-1])
-  # bind.y <- data.matrix(bindxy[1])
-  # xtx <- t(bind.x) %*% bind.x
-  # xty <- t(bind.x) %*% bind.y
-  # 
-  # sum.y <- sum(bind.y)
-  # n.rows <- nrow(bind.y)
-  # 
-  # return(list(xtx = xtx, xty = xty, sum.y = sum.y, n.rows = n.rows))
+  bind.x <- data.matrix(bindxy$x)
+  bind.y <- data.matrix(bindxy$y)
   
-  return(bindxy)
+  xtx <- t(bind.x) %*% bind.x
+  xty <- t(bind.x) %*% bind.y
   
+  sum.y <- sum(bind.y)
+  n.rows <- nrow(bind.y)
+  
+  return(list(xtx = xtx, xty = xty, sum.y = sum.y, n.rows = n.rows))
 }
+
