e34dda5e9ff7a0a00fac6d101dc3e018a17ed337
Paula Silva <paula.r.silva@inesctec.pt>
2019-11-25 17:53:35 +0000

    test

--- R/individualFill.R
+++ R/individualFill.R
@@ -1,20 +1,20 @@
 #' @export
 individualFill <- function(varName, boxes) {
   
-  boxes <- as.numeric(unlist(strsplit(boxes, split = "x")))
+  boxes <- as.list(as.numeric(unlist(strsplit(boxes, split = "x"))))
   
   dataColumn <- unlist(getVarByName(varName)[,1])
   countings <- rep(0, length(boxes))
   
-  # for (val in dataColumn){
-  #   for (i in 1:length(boxes)) {
-  #     if (val > boxes[[i]][1] & val <= boxes[[i]][2]){
-  #       countings[i] <- countings[i] + 1
-  #       break()
-  #     }
-  #   }
-  # }
+  for (val in dataColumn){
+    for (i in 1:length(boxes)) {
+      if (val > boxes[[i]][1] & val <= boxes[[i]][2]){
+        countings[i] <- countings[i] + 1
+        break()
+      }
+    }
+  }
   
-  return(dataColumn)
+  return(countings)
   
 }
