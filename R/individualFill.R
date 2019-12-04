adbb004795dfa03b25ad042d6de537c77f3415c3
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-04 20:17:32 +0000

    updates

--- R/individualFill.R
+++ R/individualFill.R
@@ -1,19 +1,19 @@
 #' @export
-individualFill <- function(varName, boxes) {
+individualFill <- function(boxes) {
   
-  boxes <- as.list(as.numeric(unlist(strsplit(boxes, split = "x"))))
-  
-  dataColumn <- unlist(getVarByName(varName)[,1])
-  countings <- rep(0, length(boxes))
-  
-  for (val in dataColumn){
-    for (i in 1:length(boxes)) {
-      if (val > boxes[[i]][1] & val <= boxes[[i]][2]){
-        countings[i] <- countings[i] + 1
-        break()
-      }
-    }
-  }
+  # boxes <- as.list(as.numeric(unlist(strsplit(boxes, split = "x"))))
+  # 
+  # dataColumn <- unlist(getVarByName(varName)[,1])
+  # countings <- rep(0, length(boxes))
+  # 
+  # for (val in dataColumn){
+  #   for (i in 1:length(boxes)) {
+  #     if (val > boxes[[i]][1] & val <= boxes[[i]][2]){
+  #       countings[i] <- countings[i] + 1
+  #       break()
+  #     }
+  #   }
+  # }
   
   return(boxes)
   
