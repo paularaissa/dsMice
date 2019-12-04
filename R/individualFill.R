17823797b1d4f464f635f2a4d4051183914dd636
Paula Silva <paula.r.silva@inesctec.pt>
2019-12-04 19:44:25 +0000

    test

--- R/individualFill.R
+++ R/individualFill.R
@@ -6,15 +6,15 @@ individualFill <- function(varName, boxes) {
   dataColumn <- unlist(getVarByName(varName)[,1])
   countings <- rep(0, length(boxes))
   
-  for (val in dataColumn){
-    for (i in 1:length(boxes)) {
-      if (val > boxes[[i]][[1]] & val <= boxes[[i]][[2]]){
-        countings[i] <- countings[i] + 1
-        break()
-      }
-    }
-  }
+  # for (val in dataColumn){
+  #   for (i in 1:length(boxes)) {
+  #     if (val > boxes[[i]][[1]] & val <= boxes[[i]][[2]]){
+  #       countings[i] <- countings[i] + 1
+  #       break()
+  #     }
+  #   }
+  # }
   
-  return(countings)
+  return(boxes)
   
 }
