725c53c6ecec8f3127a7d3bd05eb67b8ff471f6a
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-04 21:49:42 +0000

    test

--- R/individualFill.R
+++ R/individualFill.R
@@ -2,19 +2,19 @@
 individualFill <- function(boxes) {
   
   boxes <- jsonlite::fromJSON(boxes, simplifyMatrix = FALSE)
-  # 
-  # dataColumn <- unlist(getVarByName(varName)[,1])
-  # countings <- rep(0, length(boxes))
-  # 
-  # for (val in dataColumn){
-  #   for (i in 1:length(boxes)) {
-  #     if (val > boxes[[i]][1] & val <= boxes[[i]][2]){
-  #       countings[i] <- countings[i] + 1
-  #       break()
-  #     }
-  #   }
-  # }
+
+  dataColumn <- unlist(getVarByName(varName)[,1])
+  countings <- rep(0, length(boxes))
+
+  for (val in dataColumn){
+    for (i in 1:length(boxes)) {
+      if (val > boxes[[i]][1] & val <= boxes[[i]][2]){
+        countings[i] <- countings[i] + 1
+        break()
+      }
+    }
+  }
   
-  return(boxes)
+  return(countings)
   
 }
