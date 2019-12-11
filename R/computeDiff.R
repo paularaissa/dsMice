6eba8596f5b70a3fa1ffe1ef9a54023d71086bdc
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-11 15:34:01 +0000

    test

--- R/computeDiff.R
+++ R/computeDiff.R
@@ -8,27 +8,27 @@ computeDiff <- function(yHatMissing, varName) {
     studysideMessage <- "ERROR: x.name must be specified as a character string"
     return(list(studysideMessage=studysideMessage))
   }
-  yHatMiss <- jsonlite::fromJSON(yHatMissing, simplifyMatrix = FALSE)
+  yHatMiss <- unlist(jsonlite::fromJSON(yHatMissing, simplifyMatrix = FALSE))
   
   naRows <- which(is.na(x))
   completeValues <- x[-naRows]
   
   idValor <- c()
   topDiff <- list()
-  # for (value in yHatMis) {
-  #   subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
-  #   # colnames(subtract) <- "dif"
-  #   # rownames(subtract) <- rownames(completeValues)
-  #   # subtract$names <- rownames(subtract)
-  #   # orderedDiff <- subtract[with(subtract, order(dif)), ]
-  #   # topDiff[[cont]] <- orderedDiff[1:m,]
-  #   #candidateMap <- sample(topDiff[,"names"], 1)
-  #   #idValor <- orderedDiff[1,"names"]
-  #   #randomValue <- xValuesComplete[candidateMap, 1]
-  #   #imputedValues[cont] <- randomValue
-  #   return(subtract)
-  #   cont <- cont + 1
-  # }
+  for (value in yHatMis) {
+    subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
+    # colnames(subtract) <- "dif"
+    # rownames(subtract) <- rownames(completeValues)
+    # subtract$names <- rownames(subtract)
+    # orderedDiff <- subtract[with(subtract, order(dif)), ]
+    # topDiff[[cont]] <- orderedDiff[1:m,]
+    #candidateMap <- sample(topDiff[,"names"], 1)
+    #idValor <- orderedDiff[1,"names"]
+    #randomValue <- xValuesComplete[candidateMap, 1]
+    #imputedValues[cont] <- randomValue
+    return(subtract)
+    cont <- cont + 1
+  }
   return(yHatMiss)
   
 }
