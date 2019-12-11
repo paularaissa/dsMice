8871f1d99dacec30f2062c97a596a61a18c42d89
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-11 15:18:26 +0000

    test

--- R/computeDiff.R
+++ R/computeDiff.R
@@ -8,10 +8,26 @@ computeDiff <- function(yHatMissing, varName) {
     studysideMessage <- "ERROR: x.name must be specified as a character string"
     return(list(studysideMessage=studysideMessage))
   }
-  estimatedMissing <- jsonlite::fromJSON(yHatMissing, simplifyMatrix = FALSE)
+  yHatMiss <- jsonlite::fromJSON(yHatMissing, simplifyMatrix = FALSE)
   
-  #completeValues <- !is.na(x)
+  naRows <- which(is.na(x))
+  completeValues <- x[-naRows]
   
-  return(list(x, estimatedMissing))
+  idValor <- c()
+  topDiff <- list()
+  for (value in yHatMis) {
+    subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
+    colnames(subtract) <- "dif"
+    rownames(subtract) <- rownames(completeValues)
+    subtract$names <- rownames(subtract)
+    orderedDiff <- subtract[with(subtract, order(dif)), ]
+    topDiff[[cont]] <- orderedDiff[1:m,]
+    #candidateMap <- sample(topDiff[,"names"], 1)
+    #idValor <- orderedDiff[1,"names"]
+    #randomValue <- xValuesComplete[candidateMap, 1]
+    #imputedValues[cont] <- randomValue
+    cont <- cont + 1
+  }
+  return(topDiff)
   
 }
