f39c189384ee3f1110a6e3227e127312d8b06b69
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-11 14:12:58 +0000

    test

--- R/computeDiff.R
+++ R/computeDiff.R
@@ -2,6 +2,15 @@
 
 computeDiff <- function(yHatMissing, varName) {
   
+  if(is.character(varName)){
+    x <- eval(parse(text=varName))
+  }else{
+    studysideMessage <- "ERROR: x.name must be specified as a character string"
+    return(list(studysideMessage=studysideMessage))
+  }
   
+  completeValues <- !is.na(x)
+  
+  return(completeValues)
   
 }
