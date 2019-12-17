c0ef6d0889f1f67fb864ee7265c143b8d562add1
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-17 15:09:46 +0000

    test

--- R/extractMinAndMaximum.R
+++ R/extractMinAndMaximum.R
@@ -1,6 +1,10 @@
 #'@export
 extractMinAndMaximum <- function(obj) {
-  test.obj <- eval(parse(text=obj))
-  return(test.obj)
+  recovery.obj <- eval(parse(text=obj))
+  
+  min <- min(revovery.obj$dif)
+  max <- max(recovery.obj$dif)
+  
+  return(list(min=min, max=max))
   
 }
