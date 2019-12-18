65b2139af6ddfdbaee145e6ffc59091a3634bc71
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-18 15:03:49 +0000

    test

--- R/computeDiff.R
+++ R/computeDiff.R
@@ -11,27 +11,27 @@ computeDiff <- function(yHatMissing, varName, m) {
   
   yHatMiss <- as.vector(unlist(jsonlite::fromJSON(yHatMissing, simplifyMatrix = FALSE)))
   naRows <- which(is.na(x))
-  completeValues <- x[-naRows,]
+  completeValues <- x[[-naRows,]]
   m <- as.numeric(m)
   
   return(completeValues)
   
-  idValor <- c()
-  topDiff <- list()
-  cont <- 1
-  for (value in yHatMiss) {
-    subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
-    colnames(subtract) <- "dif"
-    rownames(subtract) <- rownames(completeValues)
-    subtract$names <- rownames(subtract)
-    subtract$miss <- value
-    return(list(subtract, completeValues))
-    orderedDiff <- subtract[with(subtract, order(dif)), ]
-    #topDiff[[cont]] <- orderedDiff[1:m,]
-    topDiff[[cont]] <- subtract[1:m,]
-    cont <- cont + 1
-  }
-  #join <-  do.call(rbind, topDiff)
-  return(topDiff)
+  # idValor <- c()
+  # topDiff <- list()
+  # cont <- 1
+  # for (value in yHatMiss) {
+  #   subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
+  #   colnames(subtract) <- "dif"
+  #   rownames(subtract) <- rownames(completeValues)
+  #   subtract$names <- rownames(subtract)
+  #   subtract$miss <- value
+  #   return(list(subtract, completeValues))
+  #   orderedDiff <- subtract[with(subtract, order(dif)), ]
+  #   #topDiff[[cont]] <- orderedDiff[1:m,]
+  #   topDiff[[cont]] <- subtract[1:m,]
+  #   cont <- cont + 1
+  # }
+  # #join <-  do.call(rbind, topDiff)
+  # return(topDiff)
 
 }
