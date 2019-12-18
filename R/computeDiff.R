19d3e8917c1b99ad2f58e5edc3b90a63205e93c1
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-18 15:38:32 +0000

    test

--- R/computeDiff.R
+++ R/computeDiff.R
@@ -3,8 +3,7 @@
 computeDiff <- function(yHatMissing, varName, m) {
   
   if(is.character(varName)){
-    #x <- eval(parse(text=varName))
-    x <- eval(parse(D))
+    x <- eval(parse(text=varName))
   }else{
     studysideMessage <- "ERROR: x.name must be specified as a character string"
     return(list(studysideMessage=studysideMessage))
@@ -12,27 +11,23 @@ computeDiff <- function(yHatMissing, varName, m) {
   
   yHatMiss <- as.vector(unlist(jsonlite::fromJSON(yHatMissing, simplifyMatrix = FALSE)))
   naRows <- which(is.na(x))
-  #completeValues <- x[[-naRows,]]
+  completeValues <- x[-naRows]
   m <- as.numeric(m)
   
-  return(list(x,naRows))
-  
-  # idValor <- c()
-  # topDiff <- list()
-  # cont <- 1
-  # for (value in yHatMiss) {
-  #   subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
-  #   colnames(subtract) <- "dif"
-  #   rownames(subtract) <- rownames(completeValues)
-  #   subtract$names <- rownames(subtract)
-  #   subtract$miss <- value
-  #   return(list(subtract, completeValues))
-  #   orderedDiff <- subtract[with(subtract, order(dif)), ]
-  #   #topDiff[[cont]] <- orderedDiff[1:m,]
-  #   topDiff[[cont]] <- subtract[1:m,]
-  #   cont <- cont + 1
-  # }
-  # #join <-  do.call(rbind, topDiff)
-  # return(topDiff)
+  idValor <- c()
+  topDiff <- list()
+  cont <- 1
+  for (value in yHatMiss) {
+    subtract <- data.frame(abs(mapply('-', value, completeValues))) #same x values rownames
+    colnames(subtract) <- "dif"
+    rownames(subtract) <- rownames(completeValues)
+    subtract$names <- rownames(subtract)
+    subtract$miss <- value
+    orderedDiff <- subtract[with(subtract, order(dif)), ]
+    topDiff[[cont]] <- orderedDiff[1:m,]
+    cont <- cont + 1
+  }
+  join <-  do.call(rbind, topDiff)
+  return(join)
 
 }
