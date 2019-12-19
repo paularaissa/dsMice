b29997432a5b7753c798e1d0c295bf5a7a215c7d
paula.r.silva <paula.r.silva@inesctec.pt>
2019-12-19 13:18:43 +0000

    test

--- R/matchingDiffDS.R
+++ R/matchingDiffDS.R
@@ -4,7 +4,14 @@ matchingDiffDS <- function(obj, rank) {
   ranking <- jsonlite::fromJSON(rank, simplifyMatrix = FALSE) 
   recovery.obj <- eval(parse(text=obj))
   
+  idx <- 1
+  map <- list()
+  tol = 1e-5
+  for (value in ranking) {
+    map[[idx]] <- recovery.obj[(which((abs(value - recovery.obj$dif)) <= tol)),]
+    idx <- idx + 1
+  }
+  join <- do.call(rbind, map)
   
-  
-  return(list(ranking, recovery.obj))
+  return(join)
 }
